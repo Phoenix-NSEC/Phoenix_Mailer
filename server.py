9ab0f1300e79c8d44d34f0ab13affba4ee5dec13
Shreyam Maity <sm8967724231@gmail.com>
2022-08-12 13:35:33 +0530

    mail side changes

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def uploadData():
     for k, v in json.loads(jsonData).items():
         count = count + sendZohoMail(k, subject, v)
         time.sleep(10)
-    sendZohoMail('info@phoenixnsec.in', 'mailing result',jsonify({'status': 'success', 'total_entry': total_count, 'sent_to': count}))
+    sendZohoMail(json.dumps({'status': 'success', 'total_entry': total_count, 'sent_to': count}), 'mailing result','info@phoenixnsec.in')
     return make_response(jsonify({'status': 'success', 'total_entry': total_count, 'sent_to': count}), 200)
 
 
