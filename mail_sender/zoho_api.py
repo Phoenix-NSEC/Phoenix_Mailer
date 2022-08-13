4742f9f1545a5804926f0fd83527e469491af9db
Shreyam Maity <sm8967724231@gmail.com>
2022-08-13 22:59:32 +0530

    brain freeze update

--- mail_sender/zoho_api.py
+++ mail_sender/zoho_api.py
@@ -15,12 +15,12 @@ replyEmail = os.getenv('REPLY_EMAIL')
 
 
 
-def sendZohoMail(name,subject,email):
+def sendZohoMail(name,subject,email,htmlFile):
     msg = MIMEMultipart()
     msg['Subject'] = subject  # SUBJECT
     msg['From'] = 'PHOENIX - Official Tech Club of NSEC <no-reply@phoenixnsec.in>'
     msg['reply-to'] = replyEmail
-    s = open('./assets/mail.html').read()
+    s = open(htmlFile).read()
     html = Template(s).safe_substitute(name=name)
     txt = MIMEText(html, 'html')
     msg.attach(txt)
--- server.py
+++ server.py
@@ -9,18 +9,21 @@ from flask import Flask, jsonify, request, make_response
 app = Flask(__name__)
 
 subject = 'BRAIN FREEZE | PHOENIX'
+dataFile = './templates/response.html'
 
 
 @app.route('/apiV1/upload', methods=['POST'])
 def uploadData():
+    # htmlFile = './templates/brain_freeze_welcome.html'
+    htmlFile = './templates/brain_freeze_2nd_rnd.html'
     data = request.files['csv']
     jsonData = csv_praser(data.filename, data)
     total_count = len(json.loads(jsonData))
     count = 0
     for k, v in json.loads(jsonData).items():
-        count = count + sendZohoMail(k, subject, v)
+        count = count + sendZohoMail(k, subject, v,htmlFile)
         time.sleep(10)
-    sendZohoMail(json.dumps({'status': 'success', 'total_entry': total_count, 'sent_to': count}), 'mailing result','info@phoenixnsec.in')
+    sendZohoMail(json.dumps({'status': 'success', 'total_entry': total_count, 'sent_to': count}), 'mailing result','info@phoenixnsec.in',dataFile)
     return make_response(jsonify({'status': 'success', 'total_entry': total_count, 'sent_to': count}), 200)
 
 
